bc4cea1a5329013948ed5d1d59f23187c6f5c218
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2024-07-06 21:28:50 +0100

    add bootstrap to login page

--- auth/login.php
+++ auth/login.php
@@ -24,27 +24,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php require(BASE_PATH . '/partials/header.php') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-</head>
 
-<body>
-    <h2>Login</h2>
-    <form action="login.php" method="post">
-        <div>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <button type="submit">Login</button>
-    </form>
-</body>
+<h2>Login</h2>
+<form action="login.php" method="post">
+    <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+    </div>
+    <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+    </div>
+    <button class="btn btn-primary" type="submit">Login</button>
+</form>
 
-</html>
+<?php require(BASE_PATH . '/partials/footer.php') ?>
